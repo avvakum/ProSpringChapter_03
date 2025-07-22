9dffa029d12b74f9d762fbee89ee5ad9ec7ae290
AVVA <abba501@mail.ru>
2025-07-22 10:57:33 +0300

    #PeaceDeathEverywhere 22.07.25

---
+++ src/main/java/com/avva/three/methodinject/StandardLockOpener.java
@@ -0,0 +1,27 @@
+package com.avva.three.methodinject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component("standardLockOpener")
+class StandardLockOpener implements LockOpener {
+
+    private KeyHelper keyHelper;
+
+    @Autowired
+    @Qualifier("keyHelper")
+    public void setKeyHelper(KeyHelper keyHelper) {
+        this.keyHelper = keyHelper;
+    }
+
+    @Override
+    public KeyHelper getMyKeyOpener() {
+        return keyHelper;
+    }
+
+    @Override
+    public void openLock() {
+        keyHelper.open();
+    }
+}
