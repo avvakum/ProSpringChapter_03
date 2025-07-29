860b46cf67e75d8c9eb6675f67f5b24560637621
AVVA <abba501@mail.ru>
2025-07-29 11:56:07 +0300

    #PeaceDeathEverywhere 29.07.25

---
+++ src/main/java/com/avva/three/autowiring/AnotherTarget.java
@@ -0,0 +1,36 @@
+package com.avva.three.autowiring;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+@Lazy
+class AnotherTarget {
+
+    private static Logger logger = LoggerFactory.getLogger(AnotherTarget.class);
+    Foo fooOne;
+    Foo fooTwo;
+    Bar bar;
+
+    @Autowired
+    public void setFooOne(@Qualifier("foo") Foo fooOne) {
+        logger.info(" --> AnotherTarget#setFooOne(Foo) called");
+        this.fooOne = fooOne;
+    }
+
+    @Autowired
+    public void setFooTwo(@Qualifier("anotherFoo") Foo fooTwo) {
+        logger.info(" --> AnotherTarget#setFooTwo(Foo) called");
+        this.fooTwo = fooTwo;
+    }
+
+    @Autowired
+    public void setBar(Bar bar) {
+        logger.info(" --> AnotherTarget#setBar(Bar) called");
+        this.bar = bar;
+    }
+}
