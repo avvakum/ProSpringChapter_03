ed0c72e3229adc875cb0ad51194464df03fd2c81
AVVA <abba501@mail.ru>
2025-07-30 11:10:04 +0300

    #PeaceDeathEverywhere 30.07.25

---
+++ src/main/java/com/avva/three/field/Singer.java
@@ -0,0 +1,14 @@
+package com.avva.three.field;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("singer")
+class Singer {
+    @Autowired
+    private Inspiration inspirationBean;
+
+    public void sing() {
+        System.out.println("... " + inspirationBean.getLyric());
+    }
+}
