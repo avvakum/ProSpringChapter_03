74097005ef9fa46e310342296d595a56525d1016
AVVA <abba501@mail.ru>
2025-07-31 12:55:51 +0300

    #PeaceDeathEverywhere 31.07.25

---
+++ src/main/java/com/avva/three/generator/SimpleBeanNameGenerator.java
@@ -0,0 +1,18 @@
+package com.avva.three.generator;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.context.annotation.AnnotationBeanNameGenerator;
+
+import java.util.Locale;
+import java.util.UUID;
+
+class SimpleBeanNameGenerator extends AnnotationBeanNameGenerator {
+    @Override
+    protected String buildDefaultBeanName(BeanDefinition definition, BeanDefinitionRegistry registry) {
+        var beanName = definition.getBeanClassName()
+                .substring(definition.getBeanClassName().lastIndexOf(".") + 1).toLowerCase(Locale.ROOT);
+        var uid = UUID.randomUUID().toString().replace("-", "").substring(0, 8);
+        return beanName + "-" + uid;
+    }
+}
