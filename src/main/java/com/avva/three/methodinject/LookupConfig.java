c164990e2b88959e841cc8089ad2bc5587097544
AVVA <abba501@mail.ru>
2025-08-09 11:17:03 +0300

    #PeaceDeathEverywhere 09.08.25

---
+++ src/main/java/com/avva/three/methodinject/LookupConfig.java
@@ -0,0 +1,9 @@
+package com.avva.three.methodinject;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+class LookupConfig {
+}
