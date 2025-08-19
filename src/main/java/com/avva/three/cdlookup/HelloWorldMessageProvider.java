487eeec3c588139283486688612989def7301cf5
AVVA <abba501@mail.ru>
2025-08-19 13:17:47 +0300

    #PeaceDeathEverywhere 19.08.25

---
+++ src/main/java/com/avva/three/cdlookup/HelloWorldMessageProvider.java
@@ -0,0 +1,13 @@
+package com.avva.three.cdlookup;
+
+public class HelloWorldMessageProvider implements MessageProvider {
+
+    public HelloWorldMessageProvider() {
+        System.out.println(" -----> HelloWorldMessageProvider: constructor called");
+    }
+
+    @Override
+    public String getMessage() {
+        return " Hello AVVA!";
+    }
+}
